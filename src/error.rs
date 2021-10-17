3e3aed77364a520a989ba785f0308ad586446bab
Andrew Kane <andrew@ankane.org>
2021-10-17 13:30:42 -0700

    Added fmt::Display for errors

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,21 @@
 use std::ffi::NulError;
+use std::fmt;
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
     Io,
     Parameter(String),
     Unknown
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::Io => f.write_str("cannot open file"),
+            Error::Parameter(ref err) => write!(f, "{}", err.as_str()),
+            Error::Unknown => f.write_str("unknown error"),
+        }
+    }
 }
 
 impl From<NulError> for Error {
